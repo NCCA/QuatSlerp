a86f556776e155147780c348e83285a5c41afa82
jmacey <jmacey@bournemouth.ac.uk>
2016-11-23 17:17:21 +0000

    removed all the cmake stuff and updated .gitignore

--- CMakeFiles/QtNGL_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "QtNGL_automoc.cpp"
-  "CMakeFiles/QtNGL_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/QtNGL_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
